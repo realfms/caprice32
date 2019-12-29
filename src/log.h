442fb2e7fe3b724fec9dd928a0982e9876bc6c25
FMayoral <5227916+realfms@users.noreply.github.com>
2019-12-29 19:37:24 +0100

    Add support for android logging

--- src/log.h
+++ src/log.h
@@ -5,17 +5,31 @@
 
 extern bool log_verbose;
 
-#define LOG_TO(stream,level,message) stream << (level) << " " << __FILE__ << ":" << __LINE__ << " - " << message << std::endl; // NOLINT(misc-macro-parentheses): Not having parentheses around message is a feature, it allows using streams in LOG macros
+#ifdef _ANDROID_LOG_
+    #define LOG_TO(level,message) __android_log_write(level, "Caprice32", message)
 
-#define LOG_ERROR(message) LOG_TO(std::cerr, "ERROR  ", message)
-#define LOG_WARNING(message) LOG_TO(std::cerr, "WARNING", message)
-#define LOG_INFO(message) LOG_TO(std::cerr, "INFO   ", message)
-#define LOG_VERBOSE(message) if(log_verbose) { LOG_TO(std::cout, "VERBOSE", message) }
+    #define LOG_ERROR(message) LOG_TO(ANDROID_LOG_ERROR, message)
+    #define LOG_WARNING(message) LOG_TO(ANDROID_LOG_WARNING, message)
+    #define LOG_INFO(message) LOG_TO(ANDROID_LOG_INFO, message)
+    #define LOG_VERBOSE(message) if(log_verbose) { LOG_TO(ANDROID_LOG_VERBOSE, message) }
 
-#ifdef DEBUG
-#define LOG_DEBUG(message) if(log_verbose) { LOG_TO(std::cout, "DEBUG  ", message) }
+    #ifdef DEBUG
+    #define LOG_DEBUG(message) if(log_verbose) { LOG_TO(ANDROID_LOG_DEBUG, message) }
+    #else
+    #define LOG_DEBUG(message)
+    #endif
 #else
-#define LOG_DEBUG(message)
-#endif
+    #define LOG_TO(stream,level,message) stream << (level) << " " << __FILE__ << ":" << __LINE__ << " - " << message << std::endl; // NOLINT(misc-macro-parentheses): Not having parentheses around message is a feature, it allows using streams in LOG macros
 
+    #define LOG_ERROR(message) LOG_TO(std::cerr, "ERROR  ", message)
+    #define LOG_WARNING(message) LOG_TO(std::cerr, "WARNING", message)
+    #define LOG_INFO(message) LOG_TO(std::cerr, "INFO   ", message)
+    #define LOG_VERBOSE(message) if(log_verbose) { LOG_TO(std::cout, "VERBOSE", message) }
+
+    #ifdef DEBUG
+    #define LOG_DEBUG(message) if(log_verbose) { LOG_TO(std::cout, "DEBUG  ", message) }
+    #else
+    #define LOG_DEBUG(message)
+    #endif
+#endif
 #endif
